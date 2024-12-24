774a3ff241aa283034ef8c923f98d5d9e4dadb89
berdaya <berdaya.ai@gmail.com>
2024-12-24 21:35:52 +0700

    Fanny

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -28,11 +28,19 @@ const socials = [
 		handle: "Setiya Warman",
 		photo: ASSETS.photo3.src,
 	},
+	{
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/fanny-bagus-ramadhan/",
+		label: "Full-Stack Engineer",
+		handle: "Fanny Bagus",
+		photo: ASSETS.question.src,
+	},	
+	
 ];
 
 export default function TeamsPage() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0 md:py-20">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
 				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
--- libs/assets.tsx
+++ libs/assets.tsx
@@ -1,9 +1,11 @@
 import Photo1 from "@/public/images/Hakim_11.jpg";
 import Photo2 from "@/public/images/Diar_11.png";
 import Photo3 from "@/public/images/Adis_11.png";
+import Question from "@/public/images/Question.png";
 
 export const ASSETS = {
     photo1: Photo1,
     photo2: Photo2,
     photo3: Photo3,
+    question: Question,
 };
